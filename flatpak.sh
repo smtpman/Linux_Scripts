0d2cfd89e9846d7de735b0bd536401ab80427a75
smtpman <34635830+smtpman@users.noreply.github.com>
2022-03-09 21:16:58 -0600

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo shutdown -r now
