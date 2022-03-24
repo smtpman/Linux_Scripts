5f0294565332b173d153195bdd129d1be80c4739
smtpman <34635830+smtpman@users.noreply.github.com>
2022-03-23 21:17:04 -0500

    Update Maltego_install.sh

--- Maltego_install.sh
+++ Maltego_install.sh
@@ -1,3 +1,4 @@
 sudo apt-get install openjdk-11-jdk
+wget https://maltego-downloads.s3.us-east-2.amazonaws.com/linux/Maltego.v4.3.0.deb
 # cd ~/Downloads/Maltego/ #(assuming that you’ve downloaded it here)
 # dpkg –i <maltegofile>.deb
